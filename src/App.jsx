0a553876ffbba70f3643228c6f2459d273237b20
shihabmia00 <shehabuddininfo00@gmail.com>
2024-10-27 21:05:54 +0600

    setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+    return (
+        <>
+            <h1 className='font-bold text-2xl'>Hello World</h1>
+            <span className="loading loading-ring loading-xs"></span>
+            <span className="loading loading-ring loading-sm"></span>
+            <span className="loading loading-ring loading-md"></span>
+            <span className="loading loading-ring loading-lg"></span>
+        </>
+    )
+}
+
+export default App
